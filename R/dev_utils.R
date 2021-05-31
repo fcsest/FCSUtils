af6dc1b914b25570f785c8470ac40321b1fbdb5c
Fernando Souza <fernando@mercadoedu.com.br>
2021-05-31 13:59:48 -0300

    Add install_flex

--- R/dev_utils.R
+++ R/dev_utils.R
@@ -1,3 +1,131 @@
+#' @title Flexible package installation function
+#'
+#' @description A flexible installation function for packages from CRAN or from Github repositories.
+#'
+#' @param pkgs A list of packages, only names or github's repository name;
+#'
+#' @param force Logical which force install or not; \strong{
+#' ```
+#' Default: TRUE
+#' ```
+#' }
+#'
+#' @return A list of packages that have been installed and packages that have not been installed...
+#'
+#' @examples
+#' \dontrun{
+#' # Install a list of packages from CRAN or Github Repo
+#' c("ThinkR-open/fcuk",
+#'   "ThinkR-open/attachment",
+#'   "here",
+#'   "jbkunst/highcharter",
+#'   "tidyverse") |>
+#'   install_flex(force = FALSE)}
+#'
+#' @author Fernando Souza
+#'
+#' @export
+#' @importFrom remotes install_github install_cran
+#' @importFrom purrr map keep
+#' @importFrom stringr str_detect str_extract
+install_flex <- function(pkgs, force = T){
+  ui_start("FCSUtils::install_flex",
+                     "Install Packages",
+                     "Installing Github or CRAN packages from list:")
+  ui_info(r"(Installing packages below directly from CRAN \/)")
+
+  pkgs |>
+    keep(str_detect,
+         pattern = "/",
+         negate = T) |>
+    map(install_flex_cran,
+        force = force)
+
+  pkgs |>
+    keep(str_detect,
+         pattern = "/") |>
+    map(install_flex_github,
+        force = force)
+
+  ui_end("FCSUtils::install_flex")
+
+  pkgs %>%
+    {ifelse(str_detect(.,
+                       "/"),
+            str_extract(., "(?<=/).*"),
+            .)} |>
+    check_installed()
+}
+
+#' @title Check installed packages
+#'
+#' @description Check if package has installed or not.
+#'
+#' @param pkgs A list of packages to check if has installed;
+#'
+#' @return A list with installed packages and non installed packages...
+#'
+#' @examples
+#' c("fcuk",
+#'   "attachment",
+#'   "here",
+#'   "highcharter",
+#'   "tidyverse") |>
+#'   check_installed()
+#'
+#' @export
+#' @importFrom purrr keep
+check_installed <- function(pkgs) {
+  list("installed" = pkgs |> keep(~.x %in% installed.packages()),
+       "non_installed" = pkgs |> keep(~.x %not_in% installed.packages()))
+}
+
+#' @importFrom remotes install_cran
+#' @importFrom cli col_yellow style_no_color
+install_flex_cran <- function(pkg, force = T){
+    FCSUtils::ui_step("Installing the ",
+          ui_color_start("{"),
+          col_yellow(pkg),
+          ui_color_start("}"),
+          " package directly from ",
+          style_no_color("CRAN"),
+          " right now...")
+
+  pkg |>
+    install_cran(force = force)
+}
+
+#' @importFrom remotes install_github
+#' @importFrom purrr chuck
+#' @importFrom stringr str_split
+#' @importFrom cli style_no_color col_yellow style_hyperlink
+install_flex_github <- function(pkg, force = T){
+  pkg_author <- pkg |>
+    str_split("/") |>
+    unlist() |>
+    chuck(1)
+
+  pkg_name <- pkg |>
+    str_split("/") |>
+    unlist() |>
+    chuck(2)
+
+  ui_step("Installing the ",
+          ui_color_start("{"),
+          col_yellow(pkg_name),
+          ui_color_start("}"),
+          " package directly from ",
+          paste0(pkg_author,
+                 "'s Github Repository") |>
+            style_no_color() |>
+            style_hyperlink(url = paste0("https://github.com/",
+                                         pkg)),
+          " right now...")
+
+  pkg |>
+    install_github(force = force)
+}
+
 #' @export
 #' @importFrom pacman p_load
 #' @importFrom purrr map
--- R/globals_utils.R
+++ R/globals_utils.R
@@ -9,3 +9,16 @@ globalVariables(c("."))
 
 #' @importFrom magrittr %>%
 NULL
+
+#' Inverted versions of in, is.null and is.na
+#'
+#' @noRd
+#'
+#' @examples
+#' 1 %not_in% 1:10
+#' not_null(NULL)
+`%not_in%` <- Negate(`%in%`)
+
+not_null <- Negate(is.null)
+
+not_na <- Negate(is.na)
