322ef251903156a640dfcfd2ab947c93fb90829b
Fernando Souza <fernando@mercadoedu.com.br>
2021-06-18 12:04:04 -0300

    Att dev utils functions

--- R/dev_utils.R
+++ R/dev_utils.R
@@ -26,9 +26,9 @@
 #'
 #' @export
 #' @importFrom remotes install_github install_cran
-#' @importFrom purrr map keep
+#' @importFrom purrr map keep map_chr when
 #' @importFrom stringr str_detect str_extract
-install_flex <- function(pkgs, force = T){
+install_flex <- function(pkgs, force = T) {
   ui_start("FCSUtils::install_flex",
                      "Install Packages",
                      "Installing Github or CRAN packages from list:")
@@ -49,11 +49,10 @@ install_flex <- function(pkgs, force = T){
 
   ui_end("FCSUtils::install_flex")
 
-  pkgs %>%
-    {ifelse(str_detect(.,
-                       "/"),
-            str_extract(., "(?<=/).*"),
-            .)} |>
+  pkgs |>
+    map_chr(when,
+            str_detect(., "fcsest") ~ str_extract(., "(?<=/).*"),
+            ~ paste0(.)) |>
     check_installed()
 }
 
@@ -66,44 +65,73 @@ install_flex <- function(pkgs, force = T){
 #' @return A list with installed packages and non installed packages...
 #'
 #' @examples
+#' \dontrun{
 #' c("fcuk",
 #'   "attachment",
 #'   "here",
 #'   "highcharter",
 #'   "tidyverse") |>
 #'   check_installed()
+#' }
 #'
 #' @export
-#' @importFrom purrr keep
+#' @importFrom purrr keep when
 check_installed <- function(pkgs) {
   inst_pkgs <- pkgs |>
-    keep(~.x %in% installed.packages()) %>%
-    {if (length(.) >= 1) {
-       .
-     }
-     else {
-       0
-     }
-    }
+    keep(~.x %in% installed.packages()) |>
+    when(length(.) >= 1 ~ .,
+         ~ NULL)
 
   non_inst_pkgs <- pkgs |>
-    keep(~.x %not_in% installed.packages()) %>%
-    {if (length(.) >= 1) {
-       .
-     }
-     else {
-       0
-     }
+    keep(~.x %not_in% installed.packages()) |>
+    when(length(.) >= 1 ~ .,
+         ~ NULL)
+
+  if (not_null(inst_pkgs)) {
+    half_drule() |>
+      ui_color_success() |>
+      cat()
+
+    inst_pkgs |>
+      ui_success_list(header = "Installed packages: ")
+  }
+  if (not_null(inst_pkgs) & not_null(non_inst_pkgs)) {
+    half_srule() |>
+      ui_color_success() |>
+      cat("\n")
+
+    half_srule() |>
+      ui_color_error() |>
+      cat()
+  } else if (not_null(inst_pkgs) | not_null(non_inst_pkgs)) {
+    if (not_null(inst_pkgs)) {
+      half_drule() |>
+        ui_color_success() |>
+        cat()
+    } else if (not_null(non_inst_pkgs)) {
+      half_drule() |>
+        ui_color_error() |>
+        cat()
     }
+  }
+  if (not_null(non_inst_pkgs)) {
+    non_inst_pkgs |>
+      ui_error_list(header = "Non installed packages: ")
+
+    half_drule() |>
+      ui_color_error() |>
+      cat()
+  }
 
   list("installed" = inst_pkgs,
-       "non_installed" = non_inst_pkgs)
+       "non_installed" = non_inst_pkgs) |>
+    invisible()
 }
 
 #' @importFrom remotes install_cran
 #' @importFrom cli col_yellow style_no_color
-install_flex_cran <- function(pkg, force = T){
-    FCSUtils::ui_step("Installing the ",
+install_flex_cran <- function(pkg, force = T) {
+  ui_step("Installing the ",
           ui_color_start("{"),
           col_yellow(pkg),
           ui_color_start("}"),
@@ -119,7 +147,7 @@ install_flex_cran <- function(pkg, force = T){
 #' @importFrom purrr chuck
 #' @importFrom stringr str_split
 #' @importFrom cli style_no_color col_yellow style_hyperlink
-install_flex_github <- function(pkg, force = T){
+install_flex_github <- function(pkg, force = T) {
   pkg_author <- pkg |>
     str_split("/") |>
     unlist() |>
@@ -147,10 +175,9 @@ install_flex_github <- function(pkg, force = T){
 }
 
 #' @export
-#' @importFrom pacman p_load
 #' @importFrom purrr map
 #' @importFrom usethis use_package
-preset_dev <- function(){
+preset_dev <- function(force = FALSE) {
   c("devtools",
     "here",
     "tictoc",
@@ -162,8 +189,8 @@ preset_dev <- function(){
     "stringr",
     "dplyr",
     "dtplyr",
-    "data.table") %>%
-    {p_load(char = .)}
+    "data.table") |>
+    install_flex(force = force)
 
   c("devtools",
     "here",
@@ -176,28 +203,58 @@ preset_dev <- function(){
     "stringr",
     "dplyr",
     "dtplyr",
-    "data.table") %>%
+    "data.table") |>
     map(use_package)
 }
 
 #' @export
 #' @importFrom here here
-#' @importFrom purrr discard
-#' @importFrom stringr str_remove str_squish
-#' @importFrom pacman p_load
-load_dev <- function(){
-  here("DESCRIPTION") %>%
-    readLines() %>%
-    discard(str_detect,
-            pattern = "(\\:)|(\\=)|(pacman)") %>%
-    str_remove(",") %>%
-    str_squish() %>%
-    append(values = c("devtools",
-                      "usethis",
-                      "here"),
-           after = 0) %>%
-    unique() %>%
-    {p_load(char = .)}
+#' @importFrom dplyr tibble pull filter
+#' @importFrom purrr discard pluck walk
+#' @importFrom stringr str_remove_all str_squish str_split str_subset
+load_dev <- function() {
+  pkgs <- tibble("pkg_call" = here("DESCRIPTION") |>
+                   read.dcf(fields = c("Imports", "Remotes")) |>
+                   str_remove_all("(\n)|.*::") |>
+                   str_split(",") |>
+                   unlist() |>
+                   str_subset(r"{\)$}",
+                              negate = TRUE) |>
+                   str_squish() |>
+                   append(values = c("devtools",
+                                     "usethis",
+                                     "here"),
+                          after = 0) |>
+                   unique(),
+                 "pkg_name" = pkg_call |>
+                   str_remove_all(".*/"))
+
+  non_installed <- pull(pkgs,
+                        "pkg_name") |>
+    check_installed() |>
+    pluck("non_installed")
+
+  if (not_null(non_installed)) {
+    pkgs |>
+      filter(pkg_name %in% non_installed) |>
+      pull("pkg_call") |>
+      install_flex()
+  }
+
+  if (pull(pkgs,
+           "pkg_name") |>
+      check_installed() |>
+      pluck("non_installed") |>
+      is.null()) {
+  pkgs |>
+    pull("pkg_name") |>
+    walk(require,
+         character.only = TRUE)
+  }
+
+  pull(pkgs,
+       "pkg_name") |>
+    check_installed()
 }
 
 #' @export
@@ -250,14 +307,14 @@ uses_dtplyr <- function(imports){
 #' @export
 #' @importFrom here here
 #' @importFrom devtools document check
-#' @importFrom purrr discard detect_index
+#' @importFrom purrr discard detect_index when
 check_dev <- function(){
   # Removing man folder with the old documentation
-  here("man") %>%
+  here("man") |>
   unlink(recursive = TRUE)
 
   # Removing NAMESPACE file
-  here("NAMESPACE") %>%
+  here("NAMESPACE") |>
   file.remove()
 
   # Clear workspace
@@ -270,24 +327,23 @@ check_dev <- function(){
   check()
 
   # Reorder import of data.table
-  if (here("NAMESPACE") %>%
-    readLines() %>%
+  if (here("NAMESPACE") |>
+    readLines() |>
     uses_dtplyr()) {
 
-    here("NAMESPACE") %>%
-      readLines() %>%
+    here("NAMESPACE") |>
+      readLines() |>
       discard(str_detect,
-        pattern = "import\\(data.table\\)"
-      ) %>%
-      append(
-        values = "import(data.table)",
-        after = detect_index(.,
-          str_detect,
-          pattern = "import\\(dtplyr\\)"
-        )
-      ) %>%
+        pattern = r"{import\(data.table\)}"
+      ) |>
+      when(~append(.,
+                   values = "import(data.table)",
+                   after = detect_index(.,
+                                        str_detect,
+                                        pattern = r"{import\(dtplyr\)}"
+                                        )
+                   )) |>
       writeLines(here("NAMESPACE"))
-
   }
 }
 
